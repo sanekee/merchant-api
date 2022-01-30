d9b13b6981a400a622a98a53869a26225e19f4fe
carkee <keechian@gmail.com>
2022-01-30 16:51:27 +0800

    updated spec, schemas, and errors

---
+++ backend/internal/model/errors.go
@@ -0,0 +1,22 @@
+package model
+
+import "fmt"
+
+var (
+	ErrNoResults = Errorf("error: no results")
+	ErrExists    = Errorf("error: record exists")
+	ErrRequest   = Errorf("error: bad request")
+	ErrServer    = Errorf("error: internal server error")
+)
+
+type Error struct {
+	s string
+}
+
+func Errorf(s string, args ...interface{}) Error {
+	return Error{s: fmt.Sprintf(s, args...)}
+}
+
+func (err Error) Error() string {
+	return err.s
+}
